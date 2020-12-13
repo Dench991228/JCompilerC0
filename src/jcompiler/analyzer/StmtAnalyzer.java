af6d8033201676254a8eacac4bbf068f8dfca26a
Dench991228 <denghanzhi85139@163.com>
2020-12-14 01:00:33 +0800

    完成了语句解析器的框架

--- src/jcompiler/analyzer/StmtAnalyzer.java
+++ src/jcompiler/analyzer/StmtAnalyzer.java
@@ -11,7 +11,7 @@ import java.io.FileNotFoundException;
 /*
 * 语法分析器，使用OPG完成expr的分析，先完成expr部分
 * */
-public class Analyzer {
+public class StmtAnalyzer {
     /*使用的基础设施*/
     private AnalyzerUtil Util;
 
@@ -19,14 +19,48 @@ public class Analyzer {
     private ExprAnalyzer ExprAnalyzer;
 
     /*无参数创建analyzer*/
-    public Analyzer(){
+    public StmtAnalyzer(){
         super();
     }
 
     /*根据输入文件创建新的tokenizer*/
-    public Analyzer(AnalyzerUtil util) throws FileNotFoundException {
+    public StmtAnalyzer(AnalyzerUtil util) throws FileNotFoundException {
         this.Util = util;
         this.ExprAnalyzer = new ExprAnalyzer(util);
     }
 
+    /*解析运算式语句*/
+    public void analyseExpression(){
+
+    }
+
+    /*解析声明语句*/
+    public void analyseDeclStmt(){
+
+    }
+
+    /*解析if语句*/
+    public void analyseIfStmt(){
+
+    }
+
+    /*解析while语句*/
+    public void analyseWhileStmt(){
+
+    }
+
+    /*解析返回语句*/
+    public void analyseReturnStmt(){
+
+    }
+
+    /*解析语句块*/
+    public void analyseBlockStmt(){
+
+    }
+
+    /*解析空语句*/
+    public void analyseEmptyStmt(){
+
+    }
 }
